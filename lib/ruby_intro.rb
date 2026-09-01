0288526ea13bc9eefe33ad45bdc83da3e0bc40da
penglow <abdullah.emad.alabdullah@gmail.com>
2026-08-31 18:15:56 -0700

    solved some of part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,11 +3,34 @@
 # Part 1
 
 def sum(arr)
-  # YOUR CODE HERE
+  x=0
+  arr.each do |number|
+    x=x+number
+    end
+  return x
 end
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  if arr == []
+    return 0
+  end
+  if arr.length ==1
+    return arr[0]
+  end
+
+  largest1=false
+  largest2=false
+
+  arr.each do |number|
+    if largest1 == false or number>largest1 
+      largest2 = largest1
+      largest1=number
+    elsif largest2 == false or number>largest2
+      largest2=number
+    end
+  end
+
+  return largest1+largest2
 end
 
 def sum_to_n?(arr, n)
